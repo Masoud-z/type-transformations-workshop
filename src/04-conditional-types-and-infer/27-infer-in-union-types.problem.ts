56fb0d69a10d18b8e07b5b909f55195c0002a7f6
Masoud <masoud.zeinalabedini@gmail.com>
2024-02-20 16:52:28 +0330

    Finish 27-infer-in-union-types.problem

--- src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
+++ src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
@@ -1,3 +1,4 @@
+import { TaskResult } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
 const parser1 = {
@@ -10,10 +11,23 @@ const parser3 = {
   extract: () => true,
 };
 
-type GetParserResult<T> = unknown;
+// type GetParserResult<T> = T extends { parse: () => infer TFunction }
+//   ? TFunction
+//   : T extends { extract: () => infer TExtract }
+//   ? TExtract
+//   : T extends () => infer TReturn
+//   ? TReturn
+//   : never;
+
+type GetParserResult<T> = T extends
+  | { parse: () => infer TReturn }
+  | { extract: () => infer TReturn }
+  | (() => infer TReturn)
+  ? TReturn
+  : never;
 
 type tests = [
   Expect<Equal<GetParserResult<typeof parser1>, number>>,
   Expect<Equal<GetParserResult<typeof parser2>, string>>,
-  Expect<Equal<GetParserResult<typeof parser3>, boolean>>,
+  Expect<Equal<GetParserResult<typeof parser3>, boolean>>
 ];
